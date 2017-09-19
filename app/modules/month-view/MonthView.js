32ce7bb3fd2347ddd5aada433038ca3923a0dcbe
si3792 <simo.t.iliev@industria.tech>
2017-09-19 18:00:17 +0300

    More work on month view

--- app/modules/month-view/MonthView.js
+++ app/modules/month-view/MonthView.js
@@ -5,6 +5,15 @@ export default class MonthView extends Component {
   render() {
     return (
       <div className="month-view">
+        <div className="month-view-table">
+          <div className="month-view-table-head">M</div>
+          <div className="month-view-table-head">T</div>
+          <div className="month-view-table-head">W</div>
+          <div className="month-view-table-head">T</div>
+          <div className="month-view-table-head">F</div>
+          <div className="month-view-table-head">S</div>
+          <div className="month-view-table-head">S</div>
+        </div>
       </div>
     );
   }
